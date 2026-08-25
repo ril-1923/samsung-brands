798b54f1d61e63dd853418e204c66c75f1003a65
Mohamed rilwan <rilwan.207@gmail.com>
2026-08-25 12:56:13 +0530

    Delete tv-audio.js

--- tv-audio.js
+++
@@ -1,9 +0,0 @@
-import { initNavbar, initFooter, initRevealAnimations, showToast } from './shared.js'
-
-initNavbar('tv')
-initFooter()
-initRevealAnimations()
-
-document.getElementById('exploreTvBtn')?.addEventListener('click', () => {
-  showToast('Exploring Neo QLED collection...')
-})
