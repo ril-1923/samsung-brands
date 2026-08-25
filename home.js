81337192696a3ec31a08a48e1911669a73964b75
Mohamed rilwan <rilwan.207@gmail.com>
2026-08-25 12:55:57 +0530

    Delete home.js

--- home.js
+++
@@ -1,6 +0,0 @@
-import { initNavbar, initFooter, initRevealAnimations, initCounterAnimation, showToast } from './shared.js'
-
-initNavbar('home')
-initFooter()
-initRevealAnimations()
-initCounterAnimation()
