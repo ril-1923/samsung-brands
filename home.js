d1717b95652d33e7f8cedd54e4b4bf4501e13386
Mohamed rilwan <rilwan.207@gmail.com>
2026-08-25 12:48:42 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,6 @@
+import { initNavbar, initFooter, initRevealAnimations, initCounterAnimation, showToast } from './shared.js'
+
+initNavbar('home')
+initFooter()
+initRevealAnimations()
+initCounterAnimation()
